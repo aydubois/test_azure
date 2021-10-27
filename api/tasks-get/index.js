a2cd9712cd36fe0e4bf3c8b958bda8b6af4cade1
aydubois <ay.dubois@gmail.com>
2021-10-27 15:06:45 +0200

    function tasks

---
+++ api/tasks-get/index.js
@@ -0,0 +1,30 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+    const tasks = [
+        {
+            id: 1,
+            label: "🥔 Eat Potatoes 🥔",
+            status: ""
+        },
+        {
+            id: 2,
+            label: "🛏 Sleep",
+            status: ""
+        },
+        {
+            id: 3,
+            label: "</> Code",
+            status: ""
+        }
+    ];
+}
